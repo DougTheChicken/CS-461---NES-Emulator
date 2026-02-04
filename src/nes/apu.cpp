3952f269743873baf6cb7574c5e615d1baf375b3
Chris <chrisblum@gmail.com>
2026-02-04 06:34:35 -0800

    Minimum apu.cpp that builds

---
+++ src/nes/apu.cpp
@@ -0,0 +1,16 @@
+#include "nes/apu.hpp"
+
+namespace nes
+{
+    APU::APU() { reset(); }
+    void APU::reset()
+    {
+        ;
+    };
+
+    PulseChannel::PulseChannel(bool is_pulse1)
+    {
+        ;
+    }
+}
+
