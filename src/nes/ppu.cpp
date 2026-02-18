319ab1314ed5c401fef409fdfcc5f028256c31b6
Chris <chrisblum@gmail.com>
2026-02-17 19:29:04 -0800

    PPU to CPU interface initial check-in and cpu_read_ppudata() implementation.

--- src/nes/ppu.cpp
+++ src/nes/ppu.cpp
@@ -13,25 +13,41 @@ namespace nes {
     // https://www.nesdev.org/wiki/PPU_registers#PPUDATA_-_VRAM_data_($2007_read/write)
     uint8_t PPU::cpu_read_ppudata()
     {
+        // mask off upper two bits: only 14 bits are used for addressing
+        // PPU addresses $0000 – $3FFF. Failing to mask ruins mirrors of space.
         uint16_t addr = v & 0x3FFF;
         uint8_t value = 0;
 
+        // from https://www.nesdev.org/wiki/PPU_memory_map
+        // buffered reads are in the following ranges
+        // $0000–$1FFF  CHR-ROM or CHR-RAM (pattern tables)
+        // $2000–$2FFF  Nametables
+        // $3000–$3EFF  Mirror of $2000–$2EFF The PPU does not render from this address range, so negligible utility.
         if (addr < 0x3F00)
         {
             // Normal buffered VRAM read
+            // Note: VRAM reads are delayed by 1 read so
+            // we are going to return what was read last time PPU VRAM was read,
             value = data_buffer;
+            //  and we do a read to get ready to provide it the next time we do a read
             data_buffer = ppu_bus_read(addr);
         }
         else
         {
-            // Palette read: return palette immediately (no priming needed)
+            // Palette read: return palette immediately, no delayed reads
+            // $3F00-$3F1F 	Palette RAM indexes
+            // $3F20-$3FFF 	Mirrors of $3F00-$3F1
             value = ppu_bus_read(addr) & 0x3F;
 
+            // from https://www.nesdev.org/wiki/PPU_programmer_reference#Reading_palette_RAM
             // Under-the-overlay read fills the buffer "as normal"
             data_buffer = ppu_bus_read(addr & 0x2FFF);
         }
 
-        // tricky, so we only implement v increment logic once
+        // tricky, so we only implement v increment logic once and call it when needed
+        // cpu reads trigger an increment of v because $2007 is a streaming data port -- the cpu is
+        // gonna keep reading bytes until they are gone. it will either increment 1 or 32, depending on PPUCTRL but
+        // that's not really our problem. we just get ready for the next read by incrementing after we read.
         increment_v();
 
         return value;
@@ -65,6 +81,7 @@ namespace nes {
     void PPU::increment_v()
     {
         v += vram_address_increment_flag ? 32 : 1;
+        v &= 0x7FFF; // let's add some safety to keep v in bounds
     }
 
     const uint32_t* PPU::framebuffer_output() const { return framebuffer; };
