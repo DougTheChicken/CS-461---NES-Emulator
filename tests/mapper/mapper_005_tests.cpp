4b7a48fe73e3b00f46bc4b037e36660601957822
Theo <gitea@sandinwind.com>
2026-06-01 00:43:09 -0700

    update stale tests to reflect correct updated behavior

--- tests/mapper/mapper_005_tests.cpp
+++ tests/mapper/mapper_005_tests.cpp
@@ -9,19 +9,19 @@ TEST(Mapper005Test, ExRamAndMultiplierWork) {
     uint32_t mapped_addr = 0;
     uint8_t data = 0;
 
-    EXPECT_FALSE(mapper.cpuMapWrite(0x5C10, mapped_addr, 0x77));
-    EXPECT_FALSE(mapper.cpuMapWrite(0x5104, mapped_addr, 0x02));
-    EXPECT_FALSE(mapper.cpuMapRead(0x5C10, mapped_addr, data));
+    EXPECT_TRUE(mapper.cpuMapWrite(0x5C10, mapped_addr, 0x77));
+    EXPECT_TRUE(mapper.cpuMapWrite(0x5104, mapped_addr, 0x02));
+    EXPECT_TRUE(mapper.cpuMapRead(0x5C10, mapped_addr, data));
     EXPECT_EQ(data, 0x77u);
 
-    EXPECT_FALSE(mapper.cpuMapWrite(0x5205, mapped_addr, 12));
-    EXPECT_FALSE(mapper.cpuMapWrite(0x5206, mapped_addr, 34));
-    EXPECT_FALSE(mapper.cpuMapRead(0x5205, mapped_addr, data));
+    EXPECT_TRUE(mapper.cpuMapWrite(0x5205, mapped_addr, 12));
+    EXPECT_TRUE(mapper.cpuMapWrite(0x5206, mapped_addr, 34));
+    EXPECT_TRUE(mapper.cpuMapRead(0x5205, mapped_addr, data));
     EXPECT_EQ(data, 0x98u);
-    EXPECT_FALSE(mapper.cpuMapRead(0x5206, mapped_addr, data));
+    EXPECT_TRUE(mapper.cpuMapRead(0x5206, mapped_addr, data));
     EXPECT_EQ(data, 0x01u);
 
-    EXPECT_FALSE(mapper.cpuMapWrite(0x5105, mapped_addr, 0xAA));
+    EXPECT_TRUE(mapper.cpuMapWrite(0x5105, mapped_addr, 0xAA));
     EXPECT_EQ(mapper.mirrorMode(), 0xAAu);
 }
 
